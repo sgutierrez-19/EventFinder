3de899815297bc943203220adbd4ee30b0e9e5b4
Jason Osao <josako89@gmail.com>
2019-10-04 10:14:45 -0700

    modified js

---
+++ script.js
@@ -0,0 +1,36 @@
+function populate() {
+    var topleft = $.left-button1;
+   
+    var topleft = $(p-name).text(var1);
+
+    var topleft = $(p-date).text(var1);
+
+    var topleft = $(p-location).text(var1);
+
+
+    var bottomleft = $.left-button2;
+  
+    var bottomleft = $(p-name).text(var1)
+
+    var bottomleft = $(p-date).text(var1);
+
+    var bottomleft = $(p-location).text(var1);
+
+
+    var topright = $.right-button1;
+  
+    var topright = $(p-name).text(var1);
+
+    var topright = $(p-date).text(var1);
+
+    var topright = $(p-location).text(var1);
+
+
+    var bottomright = $.right-button2;
+ 
+    var bottomright = $(p-name).text(var1);
+   
+     var bottomright = $(p-date).text(var1);
+
+    var bottomright = $(p-location).text(var1);
+}
